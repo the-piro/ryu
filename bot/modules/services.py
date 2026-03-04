469e5bf8f00ee03606cbfd2c2e2a8763f6c883c5
the-piro <thezake0077@gmail.com>
2026-03-04 08:13:11 +0530

    Update services.py

--- bot/modules/services.py
+++ bot/modules/services.py
@@ -33,9 +33,9 @@ async def start(_, message):
     lang = Language()
     buttons = ButtonMaker()
     buttons.url_button(
-        lang.START_BUTTON1, "https://www.github.com/SilentDemonSD/WZML-X"
+        lang.START_BUTTON1, "https://t.me/NxTalks"
     )
-    buttons.url_button(lang.START_BUTTON2, "https://t.me/WZML_X")
+    buttons.url_button(lang.START_BUTTON2, "https://t.me/LeechBots")
     reply_markup = buttons.build_menu(2)
 
     if len(message.command) > 1 and message.command[1] == "wzmlx":
